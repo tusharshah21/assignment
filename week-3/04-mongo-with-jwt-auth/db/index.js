558be9aacc6546c00117a39e169ec567c10ba799
tushar <tusharkumarshah14394@gmail.com>
2024-03-21 08:35:11 +0530

    04 mongo jwt completed

--- week-3/04-mongo-with-jwt-auth/db/index.js
+++ week-3/04-mongo-with-jwt-auth/db/index.js
@@ -1,25 +1,42 @@
 const mongoose = require('mongoose');
-
+const express = require('express');
+const app = express();
 // Connect to MongoDB
-mongoose.connect('your-mongodb-url');
+mongoose.connect('mongodb://localhost:27017/course_selling_app2');
 
 // Define schemas
 const AdminSchema = new mongoose.Schema({
     // Schema definition here
+    username:String,
+    password:String
+
 });
 
 const UserSchema = new mongoose.Schema({
     // Schema definition here
+    username: String,
+    password: String,
+    purchasedCourses: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Course'
+    }]
 });
 
 const CourseSchema = new mongoose.Schema({
     // Schema definition here
+    title: String,
+    description: String,
+    imageLink: String,
+    price: Number
 });
 
 const Admin = mongoose.model('Admin', AdminSchema);
 const User = mongoose.model('User', UserSchema);
 const Course = mongoose.model('Course', CourseSchema);
 
+
+app.listen(3001);
+console.log('Server started on port 3000' + Date());
 module.exports = {
     Admin,
     User,
